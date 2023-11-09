fa33454ac94d80a74dec34928acaa558a433b37e
Emily Oudyk <eoudyk@gmail.com>
2023-11-09 07:51:20 -1000

    add screenshot assets, card and carousel files

---
+++ src/components/card.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card">
+    <slot name="title"></slot>
+    <slot name="content"></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: "page-card",
+};
+</script>
+
+<style scoped>
+.card {
+  box-shadow: 1px 1px 5px rgb(50, 50, 50);
+  border-radius: 4px;
+  padding: 1rem;
+  margin: 1rem 0.5rem;
+}
+</style>
---
+++ src/components/carouselBrainflix.vue
@@ -0,0 +1,57 @@
+<script setup>
+import "vue3-carousel/dist/carousel.css";
+import { Carousel, Slide, Pagination, Navigation } from "vue3-carousel";
+</script>
+
+<template>
+  <carousel :items-to-show="1">
+    <slide v-for="slide in 1" :key="slide">
+      <div class="carousel__item">
+        <img
+          class="carousel__image"
+          src="../assets/screenshots/brainflix/Brainflix-home.png"
+          alt="screenshot showing brainflix home application"
+        />
+      </div>
+    </slide>
+    <slide v-for="slide in 1" :key="slide">
+      <div class="carousel__item">
+        <img
+          class="carousel__image"
+          src="../assets/screenshots/brainflix/Brainflix-upload.png"
+          alt="screenshot showing brainflix upload page"
+        />
+      </div>
+    </slide>
+
+    <template #addons>
+      <Navigation />
+      <Pagination />
+    </template>
+  </carousel>
+</template>
+
+<script>
+export default {
+  name: "carouselBrainflix",
+};
+</script>
+
+<style>
+.carousel {
+  width: 100%;
+}
+.carousel__item {
+  width: 100%;
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.carousel__image {
+  width: 100%;
+}
+.carousel__slide {
+  padding: 10px;
+}
+</style>
